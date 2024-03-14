8f93952933497ac9ae23d275de96c98249d16876
Joseph Rana <josephranamgr7@gmail.com>
2024-03-14 14:27:21 +0545

    Debouncing input

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -1,14 +1,36 @@
 'use client'
 import { Input } from "@/components/ui/input"
 import { Label } from '@/components/ui/label'
+import { clear } from "console";
+import { useState } from "react"
+import { useDebounce } from "use-debounce";
+import { useDebouncedCallback } from "use-debounce"
 
 
-function recordKey(e : string){
-    console.log(e)
-}
 
-export default function SearchBar(){
-    return(
-            <Input type="email" placeholder="Search Products" className="w-[400px] h-[50px] bg-neutral-50" onChange={(e) => {recordKey(e.target.value)}}/>
-    )
-}
+
+const SearchBar = () => {
+     
+
+      const [searchVal , setSearchVal] = useState("")
+      
+      const  callDebounce  = useDebouncedCallback(()=> callApi() , 3000)
+       
+      function callApi(){
+            console.log("Calling Api")
+        }
+
+      function recordKey(e : string){
+          setSearchVal(e)
+          callDebounce()
+        }
+        
+        
+        return(
+            <Input type="email" placeholder="Search Products" className="w-[400px] h-[50px] bg-neutral-50" value={searchVal} onChange={(e) => {recordKey(e.target.value)}}/>
+            )
+
+        }
+
+
+export default SearchBar
